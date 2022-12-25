bb66bcf317c2639fa823b6d7b1690f091f62b675
Jeonghoon Oh <ihoon9203@gmail.com>
2022-12-25 14:32:56 +0900

    initialized settings for Naver Map usage

--- FastestRoute/ViewController.swift
+++ FastestRoute/ViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import NMapsMap
 
 class ViewController: UIViewController {
 
+	@IBOutlet var fullView: UIView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		let mapView = NMFMapView(frame: fullView.frame)
+		mapView.mapType = .basic
+		mapView.latitude = 37.5670135
+		mapView.longitude = 126.9783740
+		let utmk = NMGUtmk(x: 953935.5, y: 1952044.1)
+		let latLng = utmk.toLatLng()
+		fullView.addSubview(mapView)
 		// Do any additional setup after loading the view.
 	}
 
